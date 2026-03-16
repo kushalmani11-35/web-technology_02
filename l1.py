40cfc7eaff9d2e82ecb5e187b7078e76f183ee99
Kushal Mani S <kushalmanis1ga22cs092@gmail.com>
2026-03-16 23:08:49 +0530

    Add files via upload

---
+++ l1.py
@@ -0,0 +1,19 @@
+'''a=[10,20,50]
+a[2]=70
+print(a)
+a.insert(2,40)
+a.append(60)
+a.pop(2)
+a.remove(70)
+print(a)
+'''
+'''b=[1,2,3,[12,13,14],"alex"]
+b[3][1]=11'''
+'''print(b)
+name = (77,44,44,22,12)
+print({name[3]'''
+towel = {20,30,40,50}
+towel.add(60)
+towel.remove(40)
+towel.pop()
+print(towel)
